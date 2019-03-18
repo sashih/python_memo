5b46c06ef49e5b2669e00ec1c5b17f50ecdf3ce8
sashih <48677970+sashih@users.noreply.github.com>
2019-03-18 20:52:51 +0800

    Create np_matrix.py
    it's about matrix multiplication.

---
+++ np_matrix.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+aaa = np.array([1, 2, 3])
+
+bbb = np.array([1, 2, 3])
+
+# Usual matrix multiplication 
+print(aaa @ bbb)
+
+# Element-wise matrix multiplication 
+print(aaa * bbb)
